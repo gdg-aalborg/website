22924375d93b548a0796009f356bd90d613af41b
Paulius Gudonis <pg@neqsoft.com>
2019-12-17 22:50:20 +0100

    Removed web app related components and vue.config.js Updated chapter details
    Added publish.sh script
    Removed vue.config.js

---
+++ publish.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env sh
+
+# abort on errors
+set -e
+
+echo Building. this may take a minute...
+npm run build
+
+cd dist
+
+echo 'gdgaalborg.dk' > CNAME
+
+echo Deploying..
+git init
+git add -A
+git commit -m 'deploy'
+
+git push -f https://github.com/gdg-aalborg/website.git master:gh-pages
+
+cd -
